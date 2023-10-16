0e5f3e53fca4a1f0c9be9964ed5ef09d6bd467d0
Sebastian Mottschall <srophkor@gmail.com>
2023-10-16 18:12:36 +0200

    fix for next hooks to guarantee that state has loaded from local storage if it existed there

--- nextHooks/useDarkMode.ts
+++ nextHooks/useDarkMode.ts
@@ -2,29 +2,32 @@ import { useEffect, useCallback } from 'react';
 import useLocalStorage from './useLocalStorage';
 
 export default function useDarkMode(
-    defaultValue?: boolean,
+	defaultValue?: boolean
 ): [boolean, () => void] {
-    const [isDarkModeEnabled, setCurrentMode] = useLocalStorage(
-        'useDarkMode',
-        defaultValue,
-    );
+	const [
+		isDarkModeEnabled,
+		setCurrentMode,
+		removeStorageSetting,
+		darkModeIsLoading,
+	] = useLocalStorage('useDarkMode', defaultValue);
 
-    useEffect(() => {
-        if (isDarkModeEnabled === undefined) {
-            setCurrentMode(
-                window.matchMedia('(prefers-color-scheme: dark)').matches,
-            );
-        }
-    }, [isDarkModeEnabled, setCurrentMode]);
+	useEffect(() => {
+		if (darkModeIsLoading) return;
+		if (isDarkModeEnabled === undefined) {
+			setCurrentMode(
+				window.matchMedia('(prefers-color-scheme: dark)').matches
+			);
+		}
+	}, [isDarkModeEnabled, setCurrentMode, darkModeIsLoading]);
 
-    const toggleDarkMode = useCallback(() => {
-        setCurrentMode((current) => !current);
-    }, [setCurrentMode]);
+	const toggleDarkMode = useCallback(() => {
+		setCurrentMode((current) => !current);
+	}, [setCurrentMode]);
 
-    // For use with tailwind, required darkMode to be set to 'class' in the tailwind.config.js
-    useEffect(() => {
-        document.documentElement.classList.toggle('dark', isDarkModeEnabled);
-    }, [isDarkModeEnabled]);
+	// For use with tailwind, required darkMode to be set to 'class' in the tailwind.config.js
+	useEffect(() => {
+		document.documentElement.classList.toggle('dark', isDarkModeEnabled);
+	}, [isDarkModeEnabled]);
 
-    return [!!isDarkModeEnabled, toggleDarkMode];
+	return [!!isDarkModeEnabled, toggleDarkMode];
 }
--- nextHooks/useLocalStorage.ts
+++ nextHooks/useLocalStorage.ts
@@ -1,41 +1,45 @@
 import { useState, useEffect, useCallback } from 'react';
 
 type TTuple<T> = [
-    T | undefined,
-    React.Dispatch<React.SetStateAction<T | undefined>>,
-    () => void,
+	T | undefined,
+	React.Dispatch<React.SetStateAction<T | undefined>>,
+	() => void,
+	boolean
 ];
 
 function useLocalStorage<T>(key: string, defaultValue: T): TTuple<T> {
-    const [value, setValue] = useState<T | undefined>();
+	const [value, setValue] = useState<T | undefined>();
+	const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const jsonValue = localStorage.getItem(key);
+	useEffect(() => {
+		const jsonValue = localStorage.getItem(key);
 
-        if (jsonValue != null) {
-            // NOTE: for now, the hook just assumes that the parsed value is in fact of type T
-            // TODO: add type validation using zod
-            const parsedValue = JSON.parse(jsonValue) as T;
+		if (jsonValue != null) {
+			// NOTE: for now, the hook just assumes that the parsed value is in fact of type T
+			// TODO: add type validation using zod
+			const parsedValue = JSON.parse(jsonValue) as T;
 
-            setValue(parsedValue);
-            return;
-        }
+			setValue(parsedValue);
+			setIsLoading(false);
+			return;
+		}
 
-        setValue(defaultValue);
-    }, [key, defaultValue]);
+		setValue(defaultValue);
+		setIsLoading(false);
+	}, []);
 
-    useEffect(() => {
-        if (value !== undefined) {
-            localStorage.setItem(key, JSON.stringify(value));
-        }
-    }, [key, value]);
+	useEffect(() => {
+		if (value !== undefined) {
+			localStorage.setItem(key, JSON.stringify(value));
+		}
+	}, [key, value]);
 
-    const remove = useCallback(() => {
-        setValue(undefined);
-        localStorage.removeItem(key);
-    }, [key]);
+	const remove = useCallback(() => {
+		setValue(undefined);
+		localStorage.removeItem(key);
+	}, [key]);
 
-    return [value, setValue, remove];
+	return [value, setValue, remove, isLoading];
 }
 
 export default useLocalStorage;
