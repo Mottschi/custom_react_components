b6712e9ae1909a7bab75a29995d3cb1520aabc06
Sebastian Mottschall <srophkor@gmail.com>
2023-10-20 13:03:09 +0200

    adding the optional parameter for toggle to the non-next version of the hook

--- hooks/useDarkMode.ts
+++ hooks/useDarkMode.ts
@@ -1,19 +1,27 @@
 import { useEffect, useCallback } from 'react';
-import React from 'react';
 import useLocalStorage from './useLocalStorage';
 
-function useDarkMode(defaultValue?: boolean): [boolean, () => void] {
+function useDarkMode(
+	defaultValue?: boolean
+): [boolean, (newValue?: boolean) => void] {
 	const [isDarkModeEnabled, setCurrentMode] = useLocalStorage(
 		'useDarkMode',
 		defaultValue ??
 			window.matchMedia('(prefers-color-scheme: dark)').matches
 	);
 
-	const toggleDarkMode = useCallback(() => {
-		setCurrentMode((current) => !current);
-	}, [setCurrentMode]);
+	const toggleDarkMode = useCallback(
+		(newValue?: boolean) => {
+			if (newValue !== undefined) {
+				setCurrentMode(newValue);
+			} else {
+				setCurrentMode((current) => !current);
+			}
+		},
+		[setCurrentMode]
+	);
 
-	// For use with tailwind, required darkMode to be set to 'class' in the tailwind.config.js
+	// For use with tailwind, requires darkMode to be set to 'class' in the tailwind.config.js
 	useEffect(() => {
 		document.documentElement.classList.toggle('dark', isDarkModeEnabled);
 	}, [isDarkModeEnabled]);
